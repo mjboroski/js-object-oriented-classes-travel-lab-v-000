21a1c51348568a0f6c291b2ce07e0eed8b7bdeae
Michael Boroski <mjboroski@gmail.com>
2018-07-09 08:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
-class Driver{
+class Driver {
   constructor(name, startDate){
-    this.name = name;
-    this.startDate = new Date(startDate);
-  };
-
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
   yearsExperienceFromBeginningOf(year){
     let endDate = new Date(year, 1, 1)
     let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
