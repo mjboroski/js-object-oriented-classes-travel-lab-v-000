bdfcdbcafcaf47c6b9f198aef9fa3e90aa30bf76
Michael Boroski <mjboroski@gmail.com>
2018-07-09 08:20:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ class Driver{
     this.startDate = new Date(startDate);
   }
 
-  yearsExperienceFromBeginningOf (endDate){
-    return endDate - this.startDate;
-  }
+  yearsExperienceFromBeginningOf (year){
+    let endDate = new Date(year, 1, 1)
+    let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
+    return parseInt(totalYears)  }
 }
 
 class Route{
